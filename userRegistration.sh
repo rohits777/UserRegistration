ae4d24c0895ee97e4514f94a483cfbdf1ba01e43
rohits777 <rohitashinde95@gmail.com>
2020-09-15 16:13:07 +0530

    Added: Validation of last name is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -x
 echo "Welcome to User Registration Program"
 read -p "Enter Your First Name:" name
+read -p "Enter your Last Name:" lastname
 regex="^[A-Z][a-z]{3,20}$"
- if [[ $name =~ $regex ]]
+ if [[ $name =~ $regex && $lastname =~ $regex ]]
    then
-      echo "Valid Name"
+      echo "Valid Name and last name"
    else
-      echo "Invalid Name"
+      echo "Invalid Name and last name"
  fi
