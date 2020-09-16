6a6911edcad4c718b558ce9b3ef936bdba9a28f3
rohits777 <rohitashinde95@gmail.com>
2020-09-16 13:31:02 +0530

    Added:Password validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -4,11 +4,11 @@ read -p "Enter Your First Name:" name
 read -p "Enter your Last Name:" lastname
 read -p "Enter your Email:" mail
 read -p "Enter your Mobile No:" mobile
-
+read -p "Enter Your Password" password
 regex="^[A-Z][a-z]{3,20}$"
 mailregex="^([a-zA-Z_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
 mobileregex="^(\+91(-)?|91(-)?|0(-)?)?(\d{10}){1}?$"
-
+passwordregex="^(?=.*\d)(?=.*[a-z])(?=.*[@$!%*?&])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
 #For First name and last name
  if [[ $name =~ $regex && $lastname =~ $regex ]]
    then
@@ -31,4 +31,12 @@ if [[ $mobile=~$mobileregex ]]
       echo "Valid Mobile Number"
   else
       echo "Invalid Mobile Number"
+fi
+
+#For password Validation
+if [[ $password=~$passwordregex ]]
+   then
+      echo "Valid Password"
+   else
+      echo "Invalid Password"
 fi
