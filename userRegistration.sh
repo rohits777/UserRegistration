55fb292f435e0d4a58bcb2e7b4fc0f196c5112ab
rohits777 <rohitashinde95@gmail.com>
2020-09-16 12:06:14 +0530

    Added: Mobile Number validation is added

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,11 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Welcome to User Registration Program"
 read -p "Enter Your First Name:" name
 read -p "Enter your Last Name:" lastname
-read -p "Enter your Email" mail
-regex="^[A-Z][a-z]{3,20}$"
-mailregex="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+read -p "Enter your Email:" mail
+read -p "Enter your Mobile No:" mobile
 
+regex="^[A-Z][a-z]{3,20}$"
+mailregex="^([a-zA-Z_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
+mobileregex="^(\+91(-)?|91(-)?|0(-)?)?(\d{10}){1}?$"
+
+#For First name and last name
  if [[ $name =~ $regex && $lastname =~ $regex ]]
    then
       echo "Valid Name and last name"
@@ -13,9 +17,18 @@ mailregex="^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$"
       echo "Invalid Name and last name"
  fi
 
+#For Email Validation
 if [[ $mail =~ $mailregex ]]
 then
         echo "valid Email"
 else
         echo "invalid Email"
+fi
+
+#For Mobile Validation
+if [[ $mobile=~$mobileregex ]]
+  then
+      echo "Valid Mobile Number"
+  else
+      echo "Invalid Mobile Number"
 fi
